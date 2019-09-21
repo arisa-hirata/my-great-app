7bfec1ff381030706d63c6860e36b7385ade5e36
“arisa-hirata” <arisa.pirata@gmail.com>
2019-09-21 15:31:20 -0700

    changed yml

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -16,7 +16,7 @@ const AsideImageOuter = styled.figure`
 const HomePage: React.FC = () => {
   return (
     <DefaultLayout className="HomePage">
-      <h1>HEEEEEEY!!!!</h1>
+      <h1>LALALALALA!!!!</h1>
       <p>
         Hi! My name is Ginpei Takanashi. I write JavaScript. I make
         JapaneseSushi.
